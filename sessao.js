8f4b1d5e86d091b54b7472ab87cc64395b36106a
Luan <rluan95@hotmail.com>
2024-08-26 11:32:05 -0300

    adicionando sessao

---
+++ sessao.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const sessao = props => {
+  return (
+    <div>sessao</div>
+  )
+}
+
+sessao.propTypes = {}
+
+export default sessao
